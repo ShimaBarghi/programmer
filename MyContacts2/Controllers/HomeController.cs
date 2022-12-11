0f68f7481aca9279c593cc2e52a2a310ef8e603c
unity <User@User9.iau.org>
2022-12-11 11:51:13 +0330

    first commit

---
+++ MyContacts2/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyContacts2.Models;
+
+
+namespace MyContacts2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(Repository.people);
+        }
+        public ActionResult Detail(int id)
+        {
+            //Person p1 = new Person();
+            //p1.Name = "shima barghi";
+            //p1.Email = "shima@yahoo.com";
+            //p1.Website = "TopLearn.com";
+            Person p = Repository.people.Single(person => person.ID == id);
+            return View(p);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Person person)
+        {
+            person.ID = Repository.people.Max(p => p.ID) + 1;
+            Repository.people.Add(person);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(int id)
+        {
+            Person person = Repository.people.SingleOrDefault(p => p.ID == id);
+            if (person == null)
+                return HttpNotFound();
+            return View(person);
+        }
+        [HttpPost]
+        public ActionResult Edit(Person person)
+        {
+            int index = Repository.people.FindIndex(p => p.ID==person.ID);
+            Repository.people[index]=person;
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            int index = Repository.people.FindIndex(p => p.ID == id);
+            Repository.people.RemoveAt(index);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MyContacts2/Models/Repository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MyContacts2.Models;
+
+namespace MyContacts2.Models
+{
+    public static class Repository
+    {
+        public static List<Person> people = new List<Person>() {
+            new Person(){ID=1,FullName="elmira arjmand",Email="elmira@yahoo.com",Website="Toplearn.com",Mobail="09306438379" },
+            new Person(){ID=2,FullName="setareh rezaei",Email="setareh@yahoo.com",Website="yalleoo.com",Mobail="09356438379" },
+            new Person(){ID=3,FullName="mona bahrami",Email="mona@yahoo.com",Website="barnamenevisan.com",Mobail="09366438379" },
+        };
+        
+        
+        
+            
+        
+
+    }
+}
